db0f054fdfd35a979f7d51179a0b0b9e9389b474
bigbn <bigbn@mail.ru>
2012-11-19 15:44:21 +0400

    smooth volume

--- japi/views.py
+++ japi/views.py
@@ -14,7 +14,7 @@ def volume(request):
             client.timeout = 10
             client.idletimeout = None
             client.connect(Grooplayer.settings.MPD_SERVER,Grooplayer.settings.MPD_PORT)
-            client.setvol(volume)
+            client.setvol(80+int(float(volume)*0.2))
             client.close()
             client.disconnect()
             
